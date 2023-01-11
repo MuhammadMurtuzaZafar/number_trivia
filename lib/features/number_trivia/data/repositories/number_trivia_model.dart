f9deb925dacc80f996b1a11fa2236bb0fa6e0540
Murtuza Zafar <murtza.zafar12345@gmail.com>
2023-01-11 15:58:11 +0500

    data repo seta

--- lib/features/number_trivia/data/repositories/number_trivia_model.dart
+++
@@ -1,7 +0,0 @@
-import 'package:number_trivia/features/number_trivia/domain/entities/number_trivia.dart';
-
-class NumberTriviaModel extends NumberTrivia{
-  NumberTriviaModel({required super.text, required super.number});
-
-
-}
